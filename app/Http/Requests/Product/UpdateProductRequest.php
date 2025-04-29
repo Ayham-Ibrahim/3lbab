5be21afabcfff5d1b94ae241199b90085cb1ad21
YousefSaleh1 <148710592+YousefSaleh1@users.noreply.github.com>
2025-04-30 00:01:33 +0300

    add prepareForValidation in StoreMyProductRequest and UpdateProductRequest

--- app/Http/Requests/Product/UpdateProductRequest.php
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -16,6 +16,38 @@ class UpdateProductRequest extends BaseFormRequest
         return true;
     }
 
+    protected function prepareForValidation()
+    {
+        Log::info('Request Data:', $this->all());
+
+        // Force convert is_available to boolean
+        if ($this->has('is_available')) {
+            $this->merge([
+                'is_available' => filter_var($this->input('is_available'), FILTER_VALIDATE_BOOLEAN),
+            ]);
+        }
+
+        // Check if variants is a JSON string and decode it into an array
+        if ($this->has('variants') && is_string($this->input('variants'))) {
+            // Remove any extra slashes and decode the JSON string into an array
+            $variantsString = stripslashes($this->input('variants'));
+            $decodedVariants = json_decode($variantsString, true);
+
+            // If decoding is successful, merge the decoded data into the request
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $this->merge([
+                    'variants' => $decodedVariants,
+                ]);
+            } else {
+                // Optionally log the error if decoding fails
+                Log::error('Invalid JSON string for variants:', [
+                    'variants' => $this->input('variants'),
+                    'json_last_error' => json_last_error_msg(),
+                ]);
+            }
+        }
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
