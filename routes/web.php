28abdaa1d030a12079c5d5b01e15f40394fe527b
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2025-06-26 22:46:09 +0300

    fix geolocation

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/link', function () {
     try {
