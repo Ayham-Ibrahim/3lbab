2028ff8012e751f0078b1fe069a3854f150068f2
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2025-06-29 03:01:21 +0300

    fix offer form data

--- app/Http/Requests/Coupon/UpdateCouponRequest.php
+++ app/Http/Requests/Coupon/UpdateCouponRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Coupon;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
 
 class UpdateCouponRequest extends FormRequest
 {
@@ -22,7 +24,12 @@ class UpdateCouponRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'code' => 'nullable|string|unique:coupons,code|max:8',
+            'code' => [
+                'nullable',
+                'string',
+                'max:8',
+                Rule::unique('coupons', 'code')->ignore($this->coupon), // <-- هذا السطر
+            ],
             'discount_percentage' => 'nullable|numeric|min:1|max:100',
             'max_uses' => 'nullable|integer|min:1',
             'expires_at' => 'nullable',
