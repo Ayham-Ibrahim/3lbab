34ff0503ab61d7ac89d0c33ea4bc6910afacfd83
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2025-07-20 00:03:39 +0300

    add notification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -107,4 +107,6 @@ return Application::configure(basePath: dirname(__DIR__))
                 return handleApiExceptions($e, $request);
             }
         });
+    })->withSchedule(function (Schedule $schedule) {
+        $schedule->command('queue:work --stop-when-empty')->everyMinute();
     })->create();
