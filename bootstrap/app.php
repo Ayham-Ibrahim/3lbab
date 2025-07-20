83554ff1692879fd112e939fd39238889e357b2f
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2025-07-20 22:37:24 +0300

    fix cron

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -100,6 +100,12 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
+
+        $middleware->redirectGuestsTo(function (Request $request) {
+            if ($request->is('api/*') || $request->expectsJson()) {
+                return null;
+            }
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (Throwable $e, Request $request) {
